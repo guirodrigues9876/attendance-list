13b359af8544c256ff5821f4ff584ff8e3a7eeaf
guilherme <guilherme121225@gmail.com>
2023-06-20 14:39:43 -0300

    State and immutability

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,15 +4,45 @@ import './styles.css';
 import { Card } from '../../components/Card';
 
 export function Home() {
+    
+    const [ studentName, setStudentName] = useState('');
+    const [students, setStudents] = useState([]);
+
+    function handleAddStudent(){
+        const newStudent = {
+            name: studentName,
+            time: new Date().toLocaleTimeString("pt-br", {
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit',
+            })
+        }
+
+        setStudents(prevState => [...prevState, newStudent]);
+    }
+
     return(
         <div className="container">
             <h1>Lista de presença</h1>
-            <input type="text" placeholder="Digite o nome..." />
-            <button type="button">Adicionar</button>
+            <input 
+                type="text" 
+                placeholder="Digite o nome..." 
+                onChange={e => setStudentName(e.target.value)}
+            
+            />
+            <button type="button" onClick={handleAddStudent}>
+                Adicionar
+            </button>
 
-            <Card name="Guilherme" time="10:55:25"/>
-            <Card name="João" time="11:00:15"/>
-
+            {   
+                students.map(student => (
+                <Card 
+                    key={student.time}
+                    name={student.name} 
+                    time={student.time}
+                />
+                ))               
+            }
         </div>
     )
 }
