c13a6bff25a9712a83c06ab75c7c5666406198dc
guilherme <guilherme121225@gmail.com>
2023-06-20 15:26:59 -0300

    useEffect Async

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './styles.css';
 import { Card } from '../../components/Card';
@@ -6,7 +6,8 @@ import { Card } from '../../components/Card';
 export function Home() {
     
     const [ studentName, setStudentName] = useState('');
-    const [students, setStudents] = useState([]);
+    const [ students, setStudents] = useState([]);
+    const [ user, setUser ] = useState({ name: '', avatar: '' });
 
     function handleAddStudent(){
         const newStudent = {
@@ -21,9 +22,29 @@ export function Home() {
         setStudents(prevState => [...prevState, newStudent]);
     }
 
+    useEffect(() => {
+        async function fetchData() {
+        const response = await fetch('https://api.github.com/users/guirodrigues9876')
+        const data = await response.json();
+
+            setUser({
+                name: data.name,
+                avatar: data.avatar_url,
+        });
+    }
+
+    fetchData();
+    }, []);
+
     return(
         <div className="container">
-            <h1>Lista de presença</h1>
+            <header>
+                <h1>Lista de presença</h1>
+                <div>
+                    <strong>{user.name}</strong>
+                    <img src={user.avatar} alt="Foto de perfil" />
+                </div>
+            </header>
             <input 
                 type="text" 
                 placeholder="Digite o nome..." 
